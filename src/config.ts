439c260f418ccc54f888f67dd36080a1e1a0d438
dhruva-antino <dhruva.v@antino.io>
2023-03-02 17:15:22 +0530

    joi added

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import bunyan from 'bunyan';
+import cloudinary from 'cloudinary';
 
 dotenv.config({});
 
@@ -11,6 +12,9 @@ class Config {
   public SECRECT_KEY_TWO: string | undefined;
   public CLIENT_URL: string | undefined;
   public REDIS_HOST: string | undefined;
+  public CLOUD_NAME: string | undefined;
+  public CLOUD_API_KEY: string | undefined;
+  public CLOUD_SECRET_KEY: string | undefined;
 
   private readonly DEFAULT_DATABSE_URL = 'mongodb://localhost:27017/chattyapp-backend';
 
@@ -22,6 +26,9 @@ class Config {
     this.SECRECT_KEY_TWO = process.env.SECRECT_KEY_TWO || '';
     this.CLIENT_URL = process.env.CLIENT_URL || '';
     this.REDIS_HOST = process.env.REDIS_HOST || '';
+    this.CLOUD_NAME = process.env.CLOUD_NAME || '';
+    this.CLOUD_API_KEY = process.env.CLOUD_API_KEY || '';
+    this.CLOUD_SECRET_KEY = process.env.CLOUD_SECRET_KEY || '';
   }
 
   public createLogger(name: string): bunyan {
@@ -34,6 +41,14 @@ class Config {
         throw new Error(`Configuration ${key} is undefined`);
       }
     }
+  }
+
+  public cloudinaryConfig():void{
+    cloudinary.v2.config({
+      cloud_name:this.CLOUD_NAME,
+      api_key:this.CLOUD_API_KEY,
+      api_secret:this.CLOUD_SECRET_KEY
+    });
   }
 }
 
